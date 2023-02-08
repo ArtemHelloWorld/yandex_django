c64a23fda2af65482e0aedfcaf5b0dbbbad69838
artem <artkon567890@gmail.com>
2023-02-08 15:25:43 +0300

    Task 3

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
-import pathlib
 import dotenv
+import pathlib
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
