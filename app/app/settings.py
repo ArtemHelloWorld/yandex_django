02541e02e533356ad2716a52d552a03ab709a801
artem <artkon567890@gmail.com>
2023-02-08 13:33:06 +0300

    Task 3

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") in ("true", "True")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 # Application definition
 
