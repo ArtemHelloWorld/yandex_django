e0189bdc284ec82f773f8758dd9247d200d035e5
Artem <artkon567890@gmail.com>
2023-02-11 22:01:20 +0300

    Task 3

--- lyceum/lyceum/asgi.py
+++ lyceum/lyceum/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lyceym project.
+ASGI config for lyceum project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lyceym.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lyceum.settings")
 
 application = get_asgi_application()
--- lyceum/lyceum/wsgi.py
+++ lyceum/lyceum/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lyceym project.
+WSGI config for lyceum project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lyceym.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lyceum.settings")
 
 application = get_wsgi_application()
