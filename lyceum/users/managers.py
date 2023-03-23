829b9b0eec94392e5b2790fc9915b6cb1e9756a3
Artem <artkon567890@gmail.com>
2023-03-23 09:35:39 +0300

    Week 7 part 3

--- lyceum/users/managers.py
+++ lyceum/users/managers.py
@@ -4,5 +4,8 @@ import django.db.models
 class MyUserManager(django.db.models.Manager):
     def get_queryset(self):
         return (
-            super(MyUserManager, self).get_queryset().select_related("profile").filter(is_active=True)
+            super(MyUserManager, self)
+            .get_queryset()
+            .select_related("profile")
+            .filter(is_active=True)
         )
