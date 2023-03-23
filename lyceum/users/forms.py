ffc0e82be2d5e60e5744226bf61d7788e523f94c
Artem <artkon567890@gmail.com>
2023-03-23 23:28:57 +0300

    Week 7 part 3

--- lyceum/users/forms.py
+++ lyceum/users/forms.py
@@ -63,21 +63,3 @@ class SignUpForm(django.contrib.auth.forms.UserCreationForm):
 
 class CustomAuthenticationForm(django.contrib.auth.forms.AuthenticationForm):
     username = django.forms.CharField(label="Логин или почта", max_length=254)
-
-    # def clean(self):
-    #     username = self.cleaned_data.get("username")
-    #     password = self.cleaned_data.get("password")
-    #
-    #     if username is not None and password:
-    #         self.user_cache = django.contrib.auth.authenticate(
-    #             self.request, username=username, password=password
-    #         )
-    #         if self.user_cache is None:
-    #             raise self.get_invalid_login_error()
-    #         else:
-    #             self.confirm_login_allowed(self.user_cache)
-    #
-    #             if "load_count" in self.request.session:
-    #                 self.request.session["load_count"] = 0
-    #
-    #     return self.cleaned_data
