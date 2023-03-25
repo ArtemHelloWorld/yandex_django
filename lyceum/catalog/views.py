ee76bd81e0668e49241f9559a9553719d0af6580
Artem <artkon567890@gmail.com>
2023-03-26 00:24:09 +0300

    week 8 part 1

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,125 +1,112 @@
 import django.db.models
 import django.http
 import django.shortcuts
+import django.views.generic
 
 import catalog.models
-
 
 NUMBER_OF_ITEMS = 5
 
 
-def item_list(request):
-    items = catalog.models.Item.objects.published(ordering="category__name")
-    context = {
-        "title": "Каталог",
-        "items": items,
-    }
-    return django.shortcuts.render(
-        request=request,
-        template_name="catalog/item_list.html",
-        context=context,
-    )
+class ItemListView(django.views.generic.ListView):
+    template_name = "catalog/item_list.html"
+    context_object_name = "items"
+
+    def get_queryset(self):
+        return catalog.models.Item.objects.published(ordering="category__name")
 
 
-def item_detail(request, item_pk):
-    item = django.shortcuts.get_object_or_404(
-        catalog.models.Item.objects.published().prefetch_related(
+class ItemDetailView(django.views.generic.DetailView):
+    template_name = "catalog/item_detail.html"
+    pk_url_kwarg = "item_pk"
+    context_object_name = "item"
+
+    def get_queryset(self):
+        return catalog.models.Item.objects.published().prefetch_related(
             "gallery",
-        ),
-        pk=item_pk,
-    )
-
-    context = {
-        "content": f"Подробно элемент. Pk:{item_pk}",
-        "item": item,
-    }
-    return django.shortcuts.render(
-        request=request,
-        template_name="catalog/item_detail.html",
-        context=context,
-    )
+        )
 
 
-def download_image_main(request, image_pk):
-    image = django.shortcuts.get_object_or_404(
-        catalog.models.ItemImageMain.objects.select_related("item"),
-        pk=image_pk,
-    )
-    if image.item.is_published:
-        image_path = image.image_main.path
-        filename = image.image_main.name.split("/")[-1]
-        image_file = open(image_path, "rb")
+class DownloadImageMainView(django.views.generic.View):
+    def get(self, request, image_pk):
+        image = django.shortcuts.get_object_or_404(
+            catalog.models.ItemImageMain.objects.select_related("item"),
+            pk=image_pk,
+        )
+        if image.item.is_published:
+            image_path = image.image_main.path
+            filename = image.image_main.name.split("/")[-1]
+            image_file = open(image_path, "rb")
 
-        response = django.http.FileResponse(image_file)
-        response["Content-Type"] = "image/jpg"
-        response["Content-Disposition"] = f"attachment; filename={filename}"
+            response = django.http.FileResponse(image_file)
+            response["Content-Type"] = "image/jpg"
+            response[
+                "Content-Disposition"
+            ] = f"attachment; filename={filename}"
 
-        return response
-    else:
-        raise django.http.Http404("This item is not published")
-
-
-def download_image_gallery(request, image_pk):
-    image = django.shortcuts.get_object_or_404(
-        catalog.models.ItemImageGallery, pk=image_pk
-    )
-    if image.item.is_published:
-        image_path = image.image_gallery.path
-        filename = image.image_gallery.name.split("/")[-1]
-        image_file = open(image_path, "rb")
-
-        response = django.http.FileResponse(image_file)
-        response["Content-Type"] = "image/jpg"
-        response["Content-Disposition"] = f"attachment; filename={filename}"
-
-        return response
-    else:
-        raise django.http.Http404("This item is not published")
+            return response
+        else:
+            raise django.http.Http404("This item is not published")
 
 
-def items_new(request):
-    items = catalog.models.Item.objects.this_week(NUMBER_OF_ITEMS)
+class DownloadImageGalleryView(django.views.generic.View):
+    def get(self, request, image_pk):
+        image = django.shortcuts.get_object_or_404(
+            catalog.models.ItemImageGallery, pk=image_pk
+        )
+        if image.item.is_published:
+            image_path = image.image_gallery.path
+            filename = image.image_gallery.name.split("/")[-1]
+            image_file = open(image_path, "rb")
 
-    context = {
-        "title": "Новинки",
-        "header": "НОВИНКИ ЭТОЙ НЕДЕЛИ!",
-        "items": items,
-    }
+            response = django.http.FileResponse(image_file)
+            response["Content-Type"] = "image/jpg"
+            response[
+                "Content-Disposition"
+            ] = f"attachment; filename={filename}"
 
-    return django.shortcuts.render(
-        request=request,
-        template_name="catalog/item_unique.html",
-        context=context,
-    )
-
-
-def items_friday(request):
-    items = catalog.models.Item.objects.friday(NUMBER_OF_ITEMS)
-
-    context = {
-        "title": "Пятница!",
-        "header": "ПЯТНИЧНЫЕ НОВИНКИ!",
-        "items": items,
-    }
-
-    return django.shortcuts.render(
-        request=request,
-        template_name="catalog/item_unique.html",
-        context=context,
-    )
+            return response
+        else:
+            raise django.http.Http404("This item is not published")
 
 
-def items_unverified(request):
-    items = catalog.models.Item.objects.unverified()
+class ItemsNewView(django.views.generic.ListView):
+    template_name = "catalog/item_unique.html"
+    context_object_name = "items"
 
-    context = {
-        "title": "Непроверенное",
-        "header": "Эти товары ещё не изменялись",
-        "items": items,
-    }
+    def get_queryset(self):
+        return catalog.models.Item.objects.this_week(NUMBER_OF_ITEMS)
 
-    return django.shortcuts.render(
-        request=request,
-        template_name="catalog/item_unique.html",
-        context=context,
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Новинки"
+        context["header"] = "НОВИНКИ ЭТОЙ НЕДЕЛИ!"
+        return context
+
+
+class ItemsFridayView(django.views.generic.ListView):
+    template_name = "catalog/item_unique.html"
+    context_object_name = "items"
+
+    def get_queryset(self):
+        return catalog.models.Item.objects.friday(NUMBER_OF_ITEMS)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Пятница!"
+        context["header"] = "ПЯТНИЧНЫЕ НОВИНКИ!"
+        return context
+
+
+class ItemsUnverifiedView(django.views.generic.ListView):
+    template_name = "catalog/item_unique.html"
+    context_object_name = "items"
+
+    def get_queryset(self):
+        return catalog.models.Item.objects.unverified()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Непроверенное"
+        context["header"] = "Эти товары ещё не изменялись"
+        return context
--- lyceum/feedback/urls.py
+++ lyceum/feedback/urls.py
@@ -5,13 +5,7 @@ import feedback.views
 app_name = "feedback"
 
 urlpatterns = [
-    django.urls.path("", feedback.views.feedback_render, name="feedback"),
     django.urls.path(
-        "check/", feedback.views.feedback_check, name="feedback_check"
-    ),
-    django.urls.path(
-        "<str:feedback_id_code>/",
-        feedback.views.feedback_detail,
-        name="feedback_detail",
+        "", feedback.views.FeedbackView.as_view(), name="feedback"
     ),
 ]
