badf2532a692b01184a0b3c1f5e2c46fce497b69
Artem <artkon567890@gmail.com>
2023-03-31 18:04:36 +0300

    rebase context_processor

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,7 +1,11 @@
+import django.contrib.auth.models
 import django.forms.models
 import django.shortcuts
 import django.test
 import django.urls
+import freezegun
+
+import users.models
 
 
 @django.test.override_settings(RATE_LIMIT_MIDDLEWARE=False)
@@ -25,3 +29,53 @@ class MyMiddlewareTestCase(django.test.TestCase):
             django.shortcuts.reverse("homepage:homepage")
         )
         self.assertEqual(response.status_code, 429)
+
+
+class BirthdayContextProcessorTest(django.test.TestCase):
+    def registrate_user(self):
+        form_data_signup = {
+            "username": "testusername",
+            "email": "testemail@mail.ru",
+            "password1": "Testpassword483",
+            "password2": "Testpassword483",
+        }
+
+        self.user = self.client.post(
+            django.shortcuts.reverse("users:signup"),
+            data=form_data_signup,
+            follow=True,
+        )
+
+    @freezegun.freeze_time("2023-01-01 00:00:01")
+    def test_birthday_today(self):
+        self.registrate_user()
+
+        user_id = django.contrib.auth.models.User.objects.get(
+            username="testusername"
+        ).id
+        profile = users.models.Profile.objects.get(user_id=user_id)
+        profile.birthday = "1984-01-01"
+        profile.save()
+
+        with freezegun.freeze_time("2023-01-01 00:30:00"):
+            response = django.test.Client().get(
+                django.urls.reverse("homepage:homepage")
+            )
+            self.assertNotEqual(len(response.context["BIRTHDAY_PEOPLE"]), 0)
+
+    @freezegun.freeze_time("2023-01-01 00:00:01")
+    def test_birthday_not_today(self):
+        self.registrate_user()
+
+        user_id = django.contrib.auth.models.User.objects.get(
+            username="testusername"
+        ).id
+        profile = users.models.Profile.objects.get(user_id=user_id)
+        profile.birthday = "2023-01-02"
+        profile.save()
+
+        with freezegun.freeze_time("2023-01-01 00:30:00"):
+            response = django.test.Client().get(
+                django.urls.reverse("homepage:homepage")
+            )
+            self.assertEqual(len(response.context["BIRTHDAY_PEOPLE"]), 0)
